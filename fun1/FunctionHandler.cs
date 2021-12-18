39c9c1f7dd30c65b0012da51de8dcf85487b51d1
Roger Johansson <rogeralsing@gmail.com>
2021-12-18 13:15:19 +0100

    .

--- fun1/FunctionHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Function
-{
-    public class FunctionHandler
-    {
-        public string Handle(string input) {
-            return $"Hi there - your input was: {input}\n";
-        }
-    }
-}
