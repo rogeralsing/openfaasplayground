be92c3c582eecdb51777d898e11be047c294f63e
Roger Johansson <rogeralsing@gmail.com>
2021-12-18 14:59:10 +0100

    .

--- fun3/Program.cs
+++ fun3/Program.cs
@@ -10,8 +10,9 @@ void Setup(WebApplicationBuilder builder)
 void Configure(WebApplication app)
 {
     // configure the HTTP request pipeline
-    app.MapPost("/", () => new { Message = "Hello" });
+    app.MapPost("/", () => new { Message = "Hello2" });
     app.MapGet("/", () => "Yo!!");
+    app.MapGet("/bar", () => "Bar!");
 }
 
 Runner.Run(args, Setup, Configure);
